123644bb203990b5c59e915fc2f2bfd9fa846664
Viktor Samuel Podhradský <samkopodhradsky@gmail.com>
2023-10-17 10:58:03 +0200

    complete few optional task from interator lesson

--- exercises/iterator/src/Client.java
+++ exercises/iterator/src/Client.java
@@ -2,13 +2,13 @@ import java.util.Iterator;
 
 public class Client {
     public static void main(String[] args) {
-        TreeNode root = new InternalNode(1,
-            new InternalNode(2,
+        TreeNode root = new InternalNode(5,
+            new InternalNode(4,
                 new LeafNode(3),
-                new LeafNode(4)),
-            new LeafNode(5));
+                new LeafNode(2)),
+            new LeafNode(1));
 
-        Iterator<TreeNode> iterator = new TreeIterable(root).iterator();
+        Iterator<TreeNode> iterator = root.orderedIterator();
 
         while (iterator.hasNext()) {
             System.out.println(iterator.next().getID());
--- exercises/iterator/src/TreeNode.java
+++ exercises/iterator/src/TreeNode.java
@@ -1,3 +1,16 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 public interface TreeNode {
     int getID();
+    default Iterator<TreeNode> orderedIterator() {
+        Iterator<TreeNode> iterator = new TreeIterable(this).iterator();
+        List<TreeNode> list = new ArrayList<>();
+        while (iterator.hasNext()) {
+            list.add(iterator.next());
+        }
+        list.sort(Comparator.comparingInt(TreeNode::getID));
+        return list.iterator();
+    }
 }
